7b9ebefc2538af9ec6891a23dba7fed6651e460a
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-04-10 16:07:52 -0300

    feat: filtro para listagem de transações

---
+++ src/MyCoreBanking.Web/Shared/Components/FiltroTransacoesModal.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace MyCoreBanking.Web.Shared.Components;
+
+public class FiltroTransacoes
+{
+    public DateTime? DataEfetivacao { get; set; }
+
+    public DateTime DataBusca { get; set; } = DateTime.Now;
+
+    public MeioPagamentoTipo? MeioPagamento { get; set; }
+
+    public OperacaoTipo? TipoOperacao { get; set; }
+
+    public TransacaoTipo? TipoTransacao { get; set; }
+
+    public Categoria? Categoria { get; set; }
+}
