767730cb2723af9d17f932caae15582a1caa484a
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-11-01 15:40:11 -0300

    feat: using AppRouter

--- src/MyCoreBanking.Web.React/src/App.tsx
+++ src/MyCoreBanking.Web.React/src/App.tsx
@@ -1,9 +1,5 @@
+import AppRouter from "./routes";
+
 export default function App() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold underline underline-offset-4 text-sky-400">
-        Hello World
-      </h1>
-    </div>
-  );
+  return <AppRouter />;
 }
