8038fcf14ec4adbec7d0960d2fadd32c30055335
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-11-02 01:24:00 -0300

    feat: config axios interceptors

--- src/MyCoreBanking.Web.React/src/services/api.ts
+++ src/MyCoreBanking.Web.React/src/services/api.ts
@@ -1,4 +1,6 @@
 import axios, { CreateAxiosDefaults } from "axios";
+import AuthTokenModel from "../interfaces/models/AuthTokenModel";
+import { toast } from "react-toastify";
 
 const defaultOptions: CreateAxiosDefaults = {
   baseURL: import.meta.env.VITE_BASE_API_URL,
@@ -8,5 +10,31 @@ const defaultOptions: CreateAxiosDefaults = {
 };
 
 const api = axios.create(defaultOptions);
+
+api.interceptors.request.use((config) => {
+  const auth: AuthTokenModel = JSON.parse(localStorage.getItem("auth") ?? "{}");
+
+  if (auth.accessToken) {
+    config.headers.Authorization = `Bearer ${auth.accessToken}`;
+  }
+
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      toast.error("Sua sessão expirou. Faça login novamente.", {
+        autoClose: 5000,
+      });
+
+      localStorage.removeItem("auth");
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default api;
