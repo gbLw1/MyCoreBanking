f2a07bdba1860fb140f0b4aad339c0bd9430e39d
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-11-05 00:09:12 -0300

    refactor: Clean layout logo

--- src/MyCoreBanking.Web.React/src/components/layouts/CleanLayout.tsx
+++ src/MyCoreBanking.Web.React/src/components/layouts/CleanLayout.tsx
@@ -1,4 +1,4 @@
-import YellowLogo from "../YellowLogo";
+import { FaPiggyBank } from "react-icons/fa6";
 
 type Props = {
   children: React.ReactNode;
@@ -9,7 +9,10 @@ export default function CleanLayout({ children }: Props) {
     <div className="h-screen w-screen bg-blue-500 flex items-center justify-center">
       <div className="max-w-[600px] w-full p-8 gap-4 bg-white rounded-md shadow-md flex flex-col justify-start">
         <header className="flex flex-col justify-center items-center gap-4">
-          <YellowLogo />
+          <FaPiggyBank
+            className="text-yellow-200 rounded-full transform rotate-[-15deg] bg-black p-3.5 mr-3"
+            size={100}
+          />
         </header>
         {children}
       </div>
