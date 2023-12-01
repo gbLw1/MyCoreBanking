7c677597eddc58a5e7b44eaefa1de45528b022ba
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-12-01 12:46:27 -0300

    feat: mobile header responsive

--- src/MyCoreBanking.Web.React/src/components/Header.tsx
+++ src/MyCoreBanking.Web.React/src/components/Header.tsx
@@ -1,20 +1,71 @@
 import { FaPiggyBank } from "react-icons/fa6";
-import { Dropdown, Navbar } from "flowbite-react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { CiLogout } from "react-icons/ci";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import clsx from "clsx";
+import { useEffect, useRef, useState } from "react";
+
+interface Page {
+  name: string;
+  path: string;
+}
+
+const pages: Page[] = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Transações",
+    path: "/transactions",
+  },
+  {
+    name: "Contas",
+    path: "/accounts",
+  },
+  {
+    name: "Perfil",
+    path: "/profile",
+  },
+  {
+    name: "Sobre nós",
+    path: "/about",
+  },
+];
 
 export default function Header() {
   const location = useLocation();
+  const path = location.pathname;
   const navigate = useNavigate();
+
+  const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
+  const dropdownTargetRef = useRef<HTMLDivElement>(null);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
   function Logout() {
     localStorage.clear();
     navigate("/login");
   }
 
+  useEffect(() => {
+    const closeDropdownOnOverlayClick = (event: MouseEvent) => {
+      if (
+        !dropdownRef.current?.contains(event.target as Node) &&
+        !dropdownTargetRef.current?.contains(event.target as Node)
+      ) {
+        setDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", closeDropdownOnOverlayClick);
+
+    return () => {
+      document.removeEventListener("mousedown", closeDropdownOnOverlayClick);
+    };
+  });
+
   return (
-    <Navbar fluid className="fixed w-full">
-      <Navbar.Brand href="/">
+    <header className="fixed px-3 py-2 w-full bg-white shadow-sm z-10 flex flex-wrap justify-between items-center">
+      <div className="flex items-center">
         <FaPiggyBank
           className="text-yellow-400 
             bg-zinc-50
@@ -28,66 +79,70 @@ export default function Header() {
         <span className="text-zinc-700 self-center whitespace-nowrap text-lg md:text-xl font-normal">
           MyCoreBanking
         </span>
-      </Navbar.Brand>
+      </div>
 
-      <div className="flex md:order-2">
-        <Dropdown
-          arrowIcon={false}
-          inline
-          label={
-            <span
-              className="me-1 md:me-0 w-8 h-8 text-white font-normal bg-transparent rounded-full
+      {/* Page links */}
+      <div className="hidden md:flex items-center gap-3">
+        {pages.map((page) => (
+          <Link
+            key={page.path}
+            to={page.path}
+            className={clsx(
+              "text-zinc-700 self-center whitespace-nowrap text-sm md:text-md font-normal",
+              path === page.path && "!text-cyan-700 !font-bold"
+            )}>
+            {page.name}
+          </Link>
+        ))}
+      </div>
+
+      {/* Dropdown */}
+      <div className="relative flex items-center">
+        <span
+          ref={dropdownTargetRef}
+          onClick={() => setDropdownOpen(!dropdownOpen)}
+          className="me-1 md:me-0 w-8 h-8 
+                text-white font-normal 
+                bg-transparent rounded-full
                 flex items-center justify-center
                 border-2 border-cyan-700
                 text-lg
                 text-zinc-700 
+                cursor-pointer
               ">
-              G
-            </span>
-          }>
-          <Dropdown.Header>
-            <span className="block text-sm font-bold mb-2">gbL</span>
-            <span className="block truncate text-sm font-medium">
-              teste@teste.com
-            </span>
-          </Dropdown.Header>
-          <Dropdown.Item onClick={Logout}>Sair</Dropdown.Item>
-        </Dropdown>
-        <Navbar.Toggle />
-      </div>
+          G
+        </span>
 
-      <Navbar.Collapse>
-        <Navbar.Link
-          href="/"
-          className={clsx(location.pathname === "/" && "font-bold")}
-          active={location.pathname === "/"}>
-          Home
-        </Navbar.Link>
-        <Navbar.Link
-          href="/transactions"
-          className={clsx(location.pathname === "/transactions" && "font-bold")}
-          active={location.pathname === "/transactions"}>
-          Transações
-        </Navbar.Link>
-        <Navbar.Link
-          href="/accounts"
-          className={clsx(location.pathname === "/accounts" && "font-bold")}
-          active={location.pathname === "/accounts"}>
-          Contas
-        </Navbar.Link>
-        <Navbar.Link
-          href="/profile"
-          className={clsx(location.pathname === "/profile" && "font-bold")}
-          active={location.pathname === "/profile"}>
-          Perfil
-        </Navbar.Link>
-        <Navbar.Link
-          href="/about"
-          className={clsx(location.pathname === "/about" && "font-bold")}
-          active={location.pathname === "/about"}>
-          Sobre nós
-        </Navbar.Link>
-      </Navbar.Collapse>
-    </Navbar>
+        <div
+          ref={dropdownRef}
+          className={clsx(
+            "absolute top-0 right-0 flex-col items-center justify-center rounded-md mt-10 border-2 border-zinc-300 me-2 w-36 md:w-28 p-2 bg-white text-black shadow-md z-10 transition-all duration-200",
+            dropdownOpen ? "opacity-100" : "opacity-0 pointer-events-none"
+          )}>
+          {/* 🟡📱 Mobile 📱🟡 -> Page links */}
+          <div className="flex md:hidden flex-col items-center justify-center gap-2">
+            {pages.map((page) => (
+              <Link
+                key={page.path}
+                to={page.path}
+                className={clsx(
+                  "text-zinc-700 self-center whitespace-nowrap text-sm md:text-md font-normal",
+                  path === page.path && "!text-cyan-700 !font-bold"
+                )}>
+                {page.name}
+              </Link>
+            ))}
+            <hr className="w-full border-gray-300 my-1" />
+          </div>
+
+          <span
+            onClick={Logout}
+            className="flex justify-center items-center cursor-pointer hover:text-cyan-700">
+            <CiLogout className="me-2" />
+            Sair
+          </span>
+        </div>
+      </div>
+    </header>
   );
 }
