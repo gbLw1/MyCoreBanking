7836eae07850194ddc0f178059a89967d196f427
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2023-12-01 10:52:18 -0300

    chore: mainlayout color

--- src/MyCoreBanking.Web.React/src/components/layouts/MainLayout.tsx
+++ src/MyCoreBanking.Web.React/src/components/layouts/MainLayout.tsx
@@ -8,7 +8,7 @@ export default function MainLayout({ children }: Props) {
   return (
     <>
       <Header />
-      <div className="min-h-screen flex flex-col items-center justify-center bg-zinc-200 text-zinc-700">
+      <div className="min-h-screen flex flex-col items-center justify-center bg-zinc-100 text-zinc-700">
         {children}
       </div>
     </>
